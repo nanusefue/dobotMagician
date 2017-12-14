be92996fbdff45821166f12a44d296279472c13d
nanusefue <nanusefue@gmail.com>
2017-12-14 17:07:33 +0100

    pins test interrupt

---
+++ Code/PythonMove/pinsout.py
@@ -0,0 +1,22 @@
+#https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+#use interruptions for call any funcion in python Scrypt
+import RPi.GPIO as GPIO
+import time
+
+class io: 
+
+    def __init__(self):
+        self.pint
+        self.modePin
+        self.channel = GPIO.wait_for_edge(self.channel, GPIO_RISING, timeout=5000)
+        GPIO.add_event_detect(self.channel, GPIO.RISING)  # add rising edge detection on a channel
+        GPIO.add_event_detect(self.channel, GPIO.RISING, callback=action)  # add rising edge detection on a channel
+
+
+    def action(channel):
+        print('This is a edge event callback function!')
+        print('Edge detected on channel %s'%self.channel)
+        print('This is run in a different thread to your main program')
+
+    def detach():
+        GPIO.remove_event_detect(self.channel)
